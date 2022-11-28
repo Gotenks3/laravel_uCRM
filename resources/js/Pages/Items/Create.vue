033cadde67bacc5e022fd800548c70433b00ba4b
Gotenks3 <riku.murakami333@gmail.com>
2022-11-28 23:43:42 +0900

    a

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -39,7 +39,7 @@ const storeItem = () => {
 											<div class="p-2 w-full">
 												<div class="relative">
 													<label for="name" class="leading-7 text-sm text-gray-600">商品名</label>
-													<input type="text" id="name" name="name" 
+													<input type="text" id="name" name="name" v-model="form.name"
 														class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
 												</div>
 											</div>
