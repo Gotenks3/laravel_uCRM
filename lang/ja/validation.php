1fa42cc748c7b5f9d7e13d4621f125961e59b98f
Gotenks3 <riku.murakami333@gmail.com>
2022-11-29 01:46:51 +0900

    顧客情報

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -154,9 +154,18 @@ return [
     */
 
     'attributes' => [
-        'name' => '商品名',
+        'title' => '件名',
+        'content' => '本文',
+        'name' => '名',
         'memo' => 'メモ',
-        'price' => '商品価格',
+        'price' => '料金',
+        'kana' => 'カナ',
+        'tel' => '電話番号',
+        'email' => 'メールアドレス',
+        'postcode' => '郵便番号',
+        'address' => '住所',
+        'birthday' => '誕生日',
+        'gender' => '性別'
     ],
 
 ];
